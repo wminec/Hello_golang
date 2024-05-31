0b5fc41dd635b37d2e4b7f9325bb96f71a3978fa
wminec <wminec@gmail.com>
2024-06-01 00:55:28 +0900

    zsdq

--- Test/OCP_Event_Watcher/main.go
+++ Test/OCP_Event_Watcher/main.go
@@ -1,57 +1,39 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
 	"path/filepath"
-	"syscall"
 
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/fields"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
-// handleEvent는 Kubernetes 이벤트를 처리하고 출력합니다.
-func handleEvent(obj interface{}) {
-	event, ok := obj.(*v1.Event)
-	if !ok {
-		log.Println("Error casting to event")
-		return
-	}
-	fmt.Printf("Event: %s %s %s\n", event.InvolvedObject.Kind, event.InvolvedObject.Name, event.Message)
-}
-
 func main() {
-	// In-cluster configuration
-	config, err := rest.InClusterConfig()
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		log.Printf("Error creating in-cluster config: %v\n", err)
-
-		// Local configuration for testing
-		var kubeconfig *string
-		if home := homedir.HomeDir(); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		}
-		flag.Parse()
-
-		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err != nil {
-			log.Fatalf("Error building kubeconfig: %v\n", err)
-		}
+		panic(err.Error())
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Error creating clientset: %v\n", err)
+		panic(err.Error())
 	}
 
 	namespace := os.Getenv("NAMESPACE")
@@ -59,34 +41,30 @@ func main() {
 		namespace = "default"
 	}
 
-	// Event watcher
-	watchlist := cache.NewListWatchFromClient(
-		clientset.CoreV1().RESTClient(),
-		"events",
-		namespace,
-		fields.Everything(),
-	)
-
-	eventHandler := cache.ResourceEventHandlerFuncs{
-		AddFunc: handleEvent,
-	}
-
 	informer := cache.NewSharedIndexInformer(
-		watchlist,
-		&v1.Event{},
-		0, // No resync period
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return clientset.CoreV1().Events(namespace).List(context.TODO(), options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return clientset.CoreV1().Events(namespace).Watch(context.TODO(), options)
+			},
+		},
+		&corev1.Event{},
+		0,
 		cache.Indexers{},
 	)
 
-	informer.AddEventHandler(eventHandler)
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			event := obj.(*corev1.Event)
+			if event.InvolvedObject.Kind == "Pod" && event.Reason == "Started" {
+				fmt.Printf("Event: %v\n", event.Message)
+			}
+		},
+	})
 
 	stop := make(chan struct{})
 	defer close(stop)
-
-	go informer.Run(stop)
-
-	// Handle graceful shutdown
-	sigterm := make(chan os.Signal, 1)
-	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
-	<-sigterm
+	informer.Run(stop)
 }
